db5b2f1665ebe2957f49035f1966198adaf52b19
ianBernard <82122990+ianBernard@users.noreply.github.com>
2022-08-12 11:40:21 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, onPress,  } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, onPress } from 'react-native';
+import React, { useEffect, useState } from 'react'; 
+import { ActivityIndicator } from 'react-native-web';
+let [isloading, setIsLoading] = useState(true); 
+let [error, setError] = useState();
+let [response, setResponse] = useState();
 
+useEffect (() => {
+  fetch("localhost:3000/rutinas")
+  .then(res => res.json)
+  .then((result) => {
+    setIsLoading(false);
+    setResponse(result);
+  }, (error => {
+    setIsLoading(false);
+    setError(error);
+  })
+  )
+}, []);
+useEffect (() => {
+  fetch("localhost:3000/usuarios")
+  .then(res => res.json)
+  .then((result) => {
+    setIsLoading(false);
+    setResponse(result);
+  }, (error => {
+    setIsLoading(false);
+    setError(error);
+  })
+  )
+}, []);
+const getUsuario = () => {
+  if (isloading){
+  return <ActivityIndicator size="large"/>
+  }
+  if (error){
+    return <Text>(error)</Text>
+  }
+  console.log(response)
+  return <View style={{
+    alignSelf:'center',
+    backgroundColor: '#DA4343',
+    height:250,
+    width:320,
+    alignItems: 'center'
+  }}
+  >
+    <Image source={require(response["url"])} style={{width: 200, height:100}}/>
+    <Text style={{color:"#ffffff", size:"24"}}>{response["NombreEjercicio"]}</Text>
+    <Text style={{color:"#ffffff", size:"15"}}>{response["Descripcion"]}</Text>
+    <Text style={{color:"#ffffff", size:"10"}}>{response["Upvotes"]}</Text>
+    <Image source={require('./assets/App/Upvote.png')} style={{height:20, width:20}}/>
+    <Image source={require('./assets/App/Downvote.png')} style={{height:20, width:20}}/>
+    <Image source={require('./assets/App/DefaultProfile.png')} style={{height:50, width:50}}/>
+    <Text style={{color:"#ffffff", size:"15"}}>{response["usuario"]}</Text>
+  </View>
+}
 
- 
 export default function App() {
-  return (
-    <View style={styles.container}>
+  const [current, setCurrent] = useState('Inicio');
+
+  const InicioPag = (
+<View style={styles.container}>
       <View 
         style={{
           backgroundColor: "white",
@@ -60,7 +116,7 @@ export default function App() {
       />
       <TouchableOpacity
         style={styles.button1}
-        onPress={pressHandler}
+        onPress={() => setCurrent(HomePag)}
       > 
         <Text style={{
             fontSize:16,
@@ -81,7 +137,30 @@ export default function App() {
       <StatusBar style="auto" />
     </View>
   );
-}
+
+  const HomePag = (
+    <View styles="container">
+      <View 
+        style={{
+          backgroundColor: "white",
+          width: 350,
+          height: "100%",
+          flex:1,
+          position: 'absolute'
+      }}>
+        {getUsuario()}
+        <View style="barradebusqueda">
+          <Image source={require('./assets/App/IconoHome.png')} style={styles.iconobarra}/>
+          <Image source={require('./assets/App/IconoBuscar.png')} style={styles.iconobarra}/>
+          <Image source={require('./assets/App/IconoCrear.png')} style={styles.iconobarra}/>
+          <Image source={require('./assets/App/IconoUsuario.png')} style={styles.iconobarra}/>
+          <Image source={require('./assets/App/IconoNotificacion.png')} style={styles.iconobarra}/>
+        </View>
+      </View>
+    </View>
+    )
+  }
+  return current === 'Inicio' ? InicioPag : current;
 
 //Styles
 
@@ -99,6 +178,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#c4c4c4',
     alignItems: 'center',
   },
+  barradebusqueda:{
+    backgroundColor: '#CFCFCF',
+    height: 20,
+    width:'100%',
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
   logo: {
     flex: 1,
     width: 220,
@@ -107,6 +193,12 @@ const styles = StyleSheet.create({
     top:30,
     left:70,
     resizeMode: 'contain'
+ },
+ iconobarra: {
+  alignSelf: 'center',
+  height:20,
+  width:20,
+  position:'relative'
  },
  button1: {
   alignItems: "center",
